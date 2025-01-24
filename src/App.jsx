6617d6fea828c87e8cb8c982bcef9c210c184b8c
FenixBA <sistemas@fenixba.com>
2025-01-24 11:57:06 -0300

    fix capital letter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Navbar from './components/NavBar';
 import { Box } from '@chakra-ui/react';
-import MainSection from './components/mainSection';
+import MainSection from './components/MainSection';
 
 function App() {
   
